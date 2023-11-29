9fcb97c12dc035f6a86656893090744ceb131625
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-11-29 10:23:22 +0200

    Create class Student
    create student's properties

---
+++ ProjectStudent/src/bg/tu_varna/sit/oop1/Student.java
@@ -0,0 +1,12 @@
+package bg.tu_varna.sit.oop1;
+
+public class Student {
+    private String name;
+    private int facultyNumber;
+    private int course; //currCourse
+    private String major;
+    private int group;
+    // status enum
+    private double averageGrade;
+
+}
